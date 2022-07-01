6af8932baf820e9588e5589cd7f321f3b2cabe82
LACROIX Mathieu <ml270906@extra.cea.fr>
2022-07-01 13:15:01 +0200

    modif carrousel

--- index.py
+++ index.py
@@ -2,10 +2,14 @@ import collections
 import streamlit as st
 import numpy as np
 import pandas as pd
-
+from utils import carroussel
 
 
 st.subheader("Qui suis-je ?")
 with open('utils/accueil.txt', encoding='utf8') as f:
     for line in f:
         st.write(line.strip())
+
+
+if __name__ == "__main__":
+    carroussel.main()
